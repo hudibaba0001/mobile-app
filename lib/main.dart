986e842f6202e4927a480d73c085bfcba6a48aae
hudibaba0001 <123251124+hudibaba0001@users.noreply.github.com>
2025-07-21 02:09:44 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'models/travel_time_entry.dart';
@@ -6,6 +7,15 @@ import 'models/location.dart';
 import 'services/migration_service.dart';
 import 'utils/constants.dart';
 import 'config/app_router.dart';
+import 'config/app_theme.dart';
+import 'providers/travel_provider.dart';
+import 'providers/location_provider.dart';
+import 'providers/search_provider.dart';
+import 'providers/filter_provider.dart';
+import 'providers/theme_provider.dart';
+import 'providers/app_state_provider.dart';
+import 'repositories/hive_travel_repository.dart';
+import 'repositories/hive_location_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,14 +41,54 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Travel Time Logger',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        // Repositories
+        Provider<HiveTravelRepository>(
+          create: (_) => HiveTravelRepository(),
+        ),
+        Provider<HiveLocationRepository>(
+          create: (_) => HiveLocationRepository(),
+        ),
+        
+        // State providers
+        ChangeNotifierProvider<ThemeProvider>(
+          create: (_) => ThemeProvider(),
+        ),
+        ChangeNotifierProvider<AppStateProvider>(
+          create: (_) => AppStateProvider(),
+        ),
+        ChangeNotifierProxyProvider<HiveTravelRepository, TravelProvider>(
+          create: (context) => TravelProvider(
+            repository: Provider.of<HiveTravelRepository>(context, listen: false),
+          ),
+          update: (context, repository, previous) => previous ?? TravelProvider(repository: repository),
+        ),
+        ChangeNotifierProxyProvider<HiveLocationRepository, LocationProvider>(
+          create: (context) => LocationProvider(
+            repository: Provider.of<HiveLocationRepository>(context, listen: false),
+          ),
+          update: (context, repository, previous) => previous ?? LocationProvider(repository: repository),
+        ),
+        ChangeNotifierProvider<SearchProvider>(
+          create: (_) => SearchProvider(),
+        ),
+        ChangeNotifierProvider<FilterProvider>(
+          create: (_) => FilterProvider(),
+        ),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, _) {
+          return MaterialApp.router(
+            title: 'Travel Time Tracker',
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeProvider.themeMode,
+            routerConfig: AppRouter.router,
+            debugShowCheckedModeBanner: false,
+          );
+        },
       ),
-      routerConfig: AppRouter.router,
     );
   }
 }
