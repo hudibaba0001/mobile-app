e11249c6f05b4775bde52d9f28f61c52162458af
hudibaba0001 <123251124+hudibaba0001@users.noreply.github.com>
2025-07-31 18:45:47 +0200

    xd

--- test_import.dart
+++
@@ -1,6 +0,0 @@
-import 'lib/services/entry_service.dart';
-
-void main() {
-  final service = EntryService();
-  print('EntryService created successfully');
-}
--- test_simple.dart
+++
@@ -1,6 +0,0 @@
-import 'lib/services/entry_service_test.dart';
-
-void main() {
-  final service = EntryServiceTest();
-  service.test();
-}
