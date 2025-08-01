9f21aa2bd4a1cb06e7323d0d794cc1734d8adc7b
hudibaba0001 <123251124+hudibaba0001@users.noreply.github.com>
2025-08-02 00:47:20 +0200

    Update settings_provider.dart

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -3,18 +3,30 @@ import 'package:hive_flutter/hive_flutter.dart';
 import '../utils/constants.dart';
 
 class SettingsProvider extends ChangeNotifier {
-  final Box _settingsBox = Hive.box(AppConstants.appSettingsBox);
+  Box? _settingsBox;
+  bool _isInitialized = false;
 
   bool _isDarkMode = false;
   bool _isFirstLaunch = true;
 
   bool get isDarkMode => _isDarkMode;
   bool get isFirstLaunch => _isFirstLaunch;
+  bool get isInitialized => _isInitialized;
 
   Future<void> init() async {
-    _isDarkMode = _settingsBox.get('isDarkMode', defaultValue: false);
-    _isFirstLaunch = _settingsBox.get('isFirstLaunch', defaultValue: true);
-    notifyListeners();
+    try {
+      _settingsBox = Hive.box(AppConstants.appSettingsBox);
+      _isDarkMode = _settingsBox!.get('isDarkMode', defaultValue: false);
+      _isFirstLaunch = _settingsBox!.get('isFirstLaunch', defaultValue: true);
+      _isInitialized = true;
+      notifyListeners();
+    } catch (e) {
+      // If box is not available yet, use defaults
+      _isDarkMode = false;
+      _isFirstLaunch = true;
+      _isInitialized = true;
+      notifyListeners();
+    }
   }
 
   Future<void> setDarkMode(bool value) async {
