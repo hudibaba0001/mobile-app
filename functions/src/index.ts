8cde28cbe361255b4269ba1aabeac11d1c8e6960
hudibaba0001 <123251124+hudibaba0001@users.noreply.github.com>
2025-08-02 23:47:20 +0200

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,4 +3,6 @@
 import * as functions from 'firebase-functions';
 import app from './app';
 
-export const api = functions.https.onRequest(app);
+export const api = functions
+  .region('europe-west3')
+  .https.onRequest(app);
