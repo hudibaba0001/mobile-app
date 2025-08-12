5dbf1a8eabfe75fcf4da332a74fd71a6d3d7a821
hudibaba0001 <123251124+hudibaba0001@users.noreply.github.com>
2025-08-13 00:20:50 +0200

    Update enhanced_history_screen.dart

--- lib/screens/enhanced_history_screen.dart
+++ lib/screens/enhanced_history_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import '../models/entry.dart';
 import '../providers/entry_provider.dart';
